af3ecbc0f83be8beec647b6c73683d40d3c11717
Emad Ehsan <emadehsan@users.noreply.github.com>
2015-12-25 22:54:22 +0500

    PHP IDE interface complete using CodeMirror

--- resources/views/ide.blade.php
+++ resources/views/ide.blade.php
@@ -6,41 +6,67 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 
-    	<script src="/js/jquery-linedtextarea.js"></script>
-    	<link href="/css/jquery-linedtextarea.css" type="text/css" rel="stylesheet" />
-
 <style>
+body {
+    margin: 0.5%;
+}
 #code {
     font-family: consolas;
 }
 </style>
+
+<!-- CodeMirror Files -->
+<!-- PATH : /cm510/mode/php/ -->
+<link rel=stylesheet href="cm510/doc/docs.css">
+<link rel="stylesheet" href="cm510/lib/codemirror.css">
+<script src="cm510/lib/codemirror.js"></script>
+<script src="cm510/addon/edit/matchbrackets.js"></script>
+<script src="cm510/mode/htmlmixed/htmlmixed.js"></script>
+<script src="cm510/mode/xml/xml.js"></script>
+<script src="cm510/mode/javascript/javascript.js"></script>
+<script src="cm510/mode/css/css.js"></script>
+<script src="cm510/mode/clike/clike.js"></script>
+<script src="cm510/mode/php/php.js"></script>
+
+<style type="text/css">
+.CodeMirror {border-top: 1px solid black; border-bottom: 1px solid black;}
+.container, body {
+    background-color: white;
+}
+</style>
+
 </head>
 <body>
 <div class="container">
 
+    <h1>elePHPant</h1>
+    <a id="admin-link" class="glyphicon glyphicon-triangle-left" href="/codes">View Scripts</a>
+    <a id="admin-link" class="pull-right" href="/auth/logout">Logout</a>
     <div class="row">
         <form class="col-md-6" id="left-pane">
-            <h3>Hack with Code</h3>
-            <textarea class="form-control lined" rows="30" id="code" placeholder="Your scripts here"></textarea>
+            <h3>PHP here</h3>
+            <textarea class="form-control lined" rows="25" id="code" placeholder="Your script here"></textarea>
 
             <hr>
             <button class="btn btn-success">Save</button>
             <button class="btn btn-primary">Execute</button>
         </form>
         <div class="col-md-6" id="right-pane">
-            right
+            <h3>Results</h3>
         </div>
     </div>
 
 </div>
 
 <script>
-$(function() {
-    $("#code").linedtextarea(
-        {selectedLine: 1}
-    );
-});
 
+var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+  lineNumbers: true,
+  matchBrackets: true,
+  mode: "application/x-httpd-php",
+  indentUnit: 4,
+  indentWithTabs: true
+});
 </script>
 
 </body>
