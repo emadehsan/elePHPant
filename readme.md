c6a86a4dc4d8f6967fe605d01ca678274265ce13
Emad Ehsan <emadehsan@users.noreply.github.com>
2015-12-27 20:08:56 +0500

    Update README.md, Views

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 # elePHPant
+
+## Components
+- Full fledged PHP IDE
+- User Authentication (Login/Register) using Laravel Easy Auth
+- Code View by admin
+- Code Execution & Security
+
+## Laravel Features Used
+- Migrations
+- Controllers, Models, Views (Blade)
+- Etc.
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
             <div class="content">
-                <div class="title">It's time chase the dreams<br>you stopped chasing</div>
+                <div class="header">
+                    <a href="/auth/login"><h2>Login</h2></a>
+                    <a href="/auth/register"><h2></h2></a>
+                </div>
+                <div class="title">It's time to chase the dreams<br>you stopped chasing</div>
             </div>
 @endsection
